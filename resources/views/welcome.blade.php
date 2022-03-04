907f4386a302d25973b91aa9810fc7a56db530d7
Your Name <yan.vi193@gmail.com>
2022-03-04 19:24:08 +0100

    stampa-film-e-scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,27 @@
-@dump($dbtab);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <main>
+        <div class="film-container">
+            @foreach ($dbtab as $item)
+                <div class="my_card-film-container">
+                    <h3>{{$item['title']}}</h3>
+
+                    <ul>
+                        <li><b>Titolo originale:</b> {{$item['original_title']}}</li>
+                        <li><b>Anno:</b> {{$item['date']}}</li>
+                        <li><b>Voto:</b> {{$item['vote']}}</li>
+                    </ul>
+                </div>
+            @endforeach
+        </div>
+    </main>
+</body>
+</html>
